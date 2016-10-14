d8591adfc81927da77184486efc4713384e51500
kyb <js@suborbital.io>
2016-10-14 18:46:15 +0200

    Initial Commit

---
+++ lazerbeamz/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lazerbeamz
+//
+//  Created by kyb on 14/10/2016.
+//  Copyright © 2016 Suborbital Softowrks Ltd. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
